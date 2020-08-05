dc4edc8e066d42bc94b9cbadbd309584bd1ea734
Jose Monagas <josetmonagas97@gmail.com>
2020-08-04 22:21:23 -0400

    Added cutscenes support

--- Map/Map.gd
+++ Map/Map.gd
@@ -1,7 +1,19 @@
 extends Spatial
 
 onready var monument1 = $Monument1
+onready var cutscenes = $Cutscenes
+onready var creature = $Creature
 
-func _ready():
-	#monument1.connect("playRewind")
-	pass
+
+func _play_cutscene(node_name, animation_name):
+	creature.force_rest = true
+	var cutscene = cutscenes.get_node(node_name)
+	cutscene.visible = true
+	cutscene.play_animation(animation_name)
+	yield(cutscene, "cutscene_ended")
+	cutscene.visible = false
+
+
+func _on_Monument1_activated():
+	_play_cutscene("PeredoSummoning", "play_movie_backwards")
+	
