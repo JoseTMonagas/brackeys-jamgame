ed8d8f4ac94ee7e2bba621d976412c1b23d7ad21
Jose Monagas <josetmonagas97@gmail.com>
2020-08-04 22:19:01 -0400

    Added virtual functions to play animations

--- Animations/Animator.gd
+++ Animations/Animator.gd
@@ -1,10 +1,27 @@
 extends Node2D
 
+signal cutscene_ended
+
 onready var animation_player = $AnimationPlayer
 
 
 func _ready():
-	animation_player.play("default")
+	if animation_player.has_animation("default"):
+		animation_player.play("default")
+
 
 func _goto(path):
 	Global.change_scene(path)
+
+
+func play_animation(animation_name):
+	animation_player.play(animation_name)
+	
+
+
+func play_animation_backwards(animation_name):
+	animation_player.play_backwards(animation_name)
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	emit_signal("cutscene_ended")
--- Animations/Monster_Summoning.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var animationPlayer = $AnimationPlayer
-
-func _ready():
-	animationPlayer.play("play_movie_backwards")
-
-func _goto(path):
-	Global.change_scene(path)
