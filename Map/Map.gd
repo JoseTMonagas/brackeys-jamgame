e5c9789f5494ea944e2e2815bf90295d1346fc21
Ismail Amer <imav97@hotmail.com>
2020-08-06 09:32:37 -0400

    Farm Functionality added

--- Map/Map.gd
+++ Map/Map.gd
@@ -4,6 +4,15 @@ onready var monument1 = $Monument1
 onready var cutscenes = $Cutscenes
 onready var creature = $Creature
 
+onready var FarmLocked = get_node("Farm/Locked")
+onready var FarmUnlocked = get_node("Farm/Unlocked")
+
+#export (NodePath) var FarmPath
+
+#func _ready():
+#	Farm = get_node(FarmPath)
+#	pass
+
 
 func _play_cutscene(node_name, animation_name):
 	creature.force_rest = true
@@ -16,4 +25,10 @@ func _play_cutscene(node_name, animation_name):
 
 func _on_Monument1_activated():
 	_play_cutscene("PeredoSummoning", "play_movie_backwards")
-	
+
+
+func _on_MonumentBlue_activated():
+	FarmUnlocked.visible = true
+	FarmLocked.visible = false
+	#_play_cutscene("PeredoSummoning", "play_movie_backwards")
+
