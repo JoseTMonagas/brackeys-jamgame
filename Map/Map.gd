ea018d8573e343407eae999c014fe7c39d490a36
Ismail Amer <imav97@hotmail.com>
2020-08-08 01:28:26 -0400

    FIX map layout

--- Map/Map.gd
+++ Map/Map.gd
@@ -28,8 +28,8 @@ func _on_Monument1_activated():
 
 func _on_MonumentBlue_activated():
 	FarmUnlocked.visible = true
+	FarmLocked.visible = false
 	get_tree().queue_delete(FarmLocked)
-#	FarmLocked.visible = false
 	#_play_cutscene("PeredoSummoning", "play_movie_backwards")
 
 func _on_MonumentGreen_activated():
