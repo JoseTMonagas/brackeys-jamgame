0f18ac3c55b9e01f47b6226926f54363aef14a6f
Jose Monagas <josetmonagas97@gmail.com>
2020-08-08 03:10:32 -0400

    Added FarmDestruction cutscene

--- Map/Map.gd
+++ Map/Map.gd
@@ -15,25 +15,33 @@ onready var RockLocked = get_node("BlockingRock")
 onready var BarsLocked = get_node("BlackBars")
 
 
-func _play_cutscene(node_name, animation_name):
+func _play_cutscene(node_name, animation_name, backwards=false):
 	creature.force_rest = true
 	var cutscene = cutscenes.get_node(node_name)
 	cutscene.visible = true
-	cutscene.play_animation(animation_name)
+	if backwards:
+		cutscene.play_animation_backwards(animation_name)
+	else:
+		cutscene.play_animation(animation_name)
 	yield(cutscene, "cutscene_ended")
 	cutscene.visible = false
+
 
 func _on_Monument1_activated():
 	_play_cutscene("PeredoSummoning", "play_movie_backwards")
 
+
 func _on_MonumentBlue_activated():
+	_play_cutscene("FarmDestruction", "PlayMovieBackwards", true)
 	FarmUnlocked.visible = true
 	get_tree().queue_delete(FarmLocked)
+
 
 func _on_MonumentGreen_activated():
 	TreeLocked.visible = false
 	get_tree().queue_delete(TreeLocked)
 	TreeUnlocked.visible = true
+
 
 func _on_MonumentYellow_activated():
 	RockLocked.translation.z = -33
@@ -41,3 +49,34 @@ func _on_MonumentYellow_activated():
 
 func _on_MonumentPink_activated():
 	get_tree().queue_delete(BarsLocked)
+
+
+func _on_Return_mouse_entered():
+	$Hover.play()
+
+
+func _on_MainMenu_mouse_entered():
+	$Hover.play()
+
+
+func _on_Quit_mouse_entered():
+	$Hover.play()
+
+
+func _on_Return_pressed():
+	$Pressed.play()
+	$Menu.visible = false
+	
+
+func _on_MainMenu_pressed():
+	$Pressed.play()
+	Global.change_scene(Global.MAIN_MENU)
+
+
+func _on_Quit_pressed():
+	$Pressed.play()
+	get_tree().quit()
+
+
+func _on_Player_toggle_menu():
+	$Menu.visible = !$Menu.visible
